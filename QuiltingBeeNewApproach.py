9cebc4179783943981b4f700ff99c3641deec5f3
Blake Leahy <leabl393@student.otago.ac.nz>
2024-04-11 16:04:55 +1200

    quilt fits to fixed canvas size. Reads from stdin.

--- QuiltingBeeNewApproach.py
+++ QuiltingBeeNewApproach.py
@@ -17,33 +17,40 @@
 # 
 #  DrawSquare function gets all points of last square to draw new square off and draws that, returning coords/positions of the corners of that new square 
 #  
-# read input with 'with open(input.txt',r')' 
-# EstimateDimensions function to calculate total widthxheight to fit the final quilt using 'scale' in squares tuple list 
-#  -starting w a base size, for each layer after the first (squares[1:]), calculate how much more space is needed bc of that layers scale 
-#  -that extra space from each layers 'scale' parameter is added to the width and height of the canvas 
-#  -ensure canvas is sized well from start 
-# 
 # so, once input is stored into a tuple list, dimensions are calculated and canvas fitted to that calculation, then 
 # the first square is drawn, with the coordinates for the corners of it being returned and stored so we know where to center 
 # the next layer of squares. Then we loop over the square tuples holding scale r g b and 
 # for each corner of the first square and for each coordinate in each corner, draw a new square at that current corner,
 # append that to some storage for the current corners
 
+# 10 april version 
+
+# Pick a display / canvas size FIXED and then scale the quilt to fit the display size 
+# scale the quilt with CalculateScaleFactor method rather than 
+# CalculateDimensions which calculated the canvas size dependant on the quilt scales. 
+# CalculateScaleFactor will scale each square drawn in the quilt to the FIXED CANVAS size instead 
+
+# ACTUALLY read from stdin, not just 'open' the input file 
+
+
 import tkinter as tk 
 import sys 
 
-# function to calculate the dimensions of the canvas from the scale inputs prior to drawing 
-def CalculateDimensions(inputTuples):
+# Function to calculate the scale factor based on dimensions quilt will occupy for fixed canvas size 
+def CalculateScaleFactor(inputTuples, canvasWidth, canvasHeight):
 
-    totalHeight = 10 
+    totalHeight = 10
     totalWidth = 10
 
     for scale,_,_,_ in inputTuples[1:]:
-        scaledSize = scale * 100 
-        totalWidth += scaledSize * 2 
+        scaledSize = scale * 100
+        totalWidth += scaledSize * 2
         totalHeight += scaledSize * 2
-
-    return totalWidth, totalHeight 
+    
+    widthScale = canvasWidth / totalWidth 
+    heightScale = canvasHeight / totalHeight 
+    
+    return min(widthScale, heightScale) 
 
 def DrawSquare(canvas, XCenter, YCenter, size, colour): 
 
@@ -59,49 +66,50 @@ def DrawSquare(canvas, XCenter, YCenter, size, colour):
     return corners
 
 
-def main(): 
+def main(inputText): 
 
     #setup 
     root = tk.Tk()
     root.title("Quilting Bee")
 
-    with open('input.txt', 'r') as f:
-        inputText = f.read()
+    # fixed canvas size rather than calculated based off quilt layers scales
+    canvasWidth = 600
+    canvasHeight = 600
 
-    lines = inputText.strip().split('\n')
-
-    inputTuples = [(float(scale), int(r), int(g), int(b)) for scale, r, g, b in (line.split() for line in lines)]
-
-    # calculate dinmensions 
-    totalWidth, totalHeight = CalculateDimensions(inputTuples)
-    canvas = tk.Canvas(root, width = totalWidth, height = totalHeight, bg='white')
+    canvas = tk.Canvas(root, width = canvasWidth, height = canvasHeight, bg='white')
     canvas.pack()
 
+    lines = inputText.strip().split('\n')
+    inputTuples = [(float(scale), int(r), int(g), int(b)) for scale, r, g, b in (line.split() for line in lines)]
+
+    newScale = CalculateScaleFactor(inputTuples, canvasWidth, canvasHeight)
+
     # draw first square 
-    XCenter = totalWidth/2
-    YCenter = totalHeight/2
-    lastCorners = [DrawSquare(canvas, XCenter, YCenter, 100 * inputTuples[0][0], f'#{inputTuples[0][1]:02x}{inputTuples[0][2]:02x}{inputTuples[0][3]:02x}')]
+    XCenter = canvasWidth/2
+    YCenter = canvasHeight/2
+    lastCorners = DrawSquare(canvas, XCenter, YCenter, 100 * inputTuples[0][0] * newScale, f'#{inputTuples[0][1]:02x}{inputTuples[0][2]:02x}{inputTuples[0][3]:02x}')
 
     #loop over input lines starting from second as first will be drawn above 
     for scale, r, g, b in inputTuples[1:]:
 
-        size = scale * 100 
-        colour = f'#{int(r):02x}{int(g):02x}{int(b):02x}'
         currentCorners = [] 
 
         for corner in lastCorners:
-            for coordinate in corner: 
 
-                XCenter, YCenter = coordinate
-                corners = DrawSquare(canvas, XCenter, YCenter, size, colour)
-                currentCorners.append(corners)
+            XCenter, YCenter = corner
+            colour = f'#{r:02x}{g:02x}{b:02x}'
+            size = scale * newScale * 100
+            corners = DrawSquare(canvas, XCenter, YCenter, size, colour)
+            currentCorners.extend(corners)
 
-        lastCorners = currentCorners 
-    
+        lastCorners = currentCorners
+
     root.mainloop()
+    
 
 if __name__ == "__main__":
-    main()
+    inputText = sys.stdin.read()
+    main(inputText)
 
 
 
